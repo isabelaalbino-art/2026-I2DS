48c70cc506fd4004a7db950422908083ef6db92e
isabelaalbino-art <isabela.albino@edu.senai.br>
2026-08-05 15:58:47 -0300

    Aula 04 - Pilares da Programação Orientada a Objetos

---
+++ Aula-04/01-Abstração/ClasseObjeto/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+      /*
+      Classe -> é a abstração de um objeto do mundo real para o o mundo computacional
+      Objeto -> é a instância de uma classe
+      */
+
+      //Instanciar um objeto do tipo Aluno
+      Aluno aluno01 = new Aluno();
+      Aluno aluno02 = new Aluno();
+
+      aluno01.Nome = "José da Silva";
+      aluno01.RM = 2444;
+      aluno01.DataNascimento = new DateOnly(2010,01,15);
+
+      aluno02.Nome = "João dos Santos";
+      aluno02.RM = 1234;
+      aluno02.DataNascimento =  new DateOnly(2006,12,07);
+
+      //Execultar métodos
+      aluno01.ApresentarSe();
+      aluno02.ApresentarSe();
+
+   
+    }
+
+// Declaração de uma classe
+    public class Aluno // Declaração de uma classe 
+    {
+        // Atributos -> Caracteristicas 
+        public string Nome {get; set; }
+        public int RM {get; set; }
+        public DateOnly DataNascimento {get; set; }
+
+        // Métodos -> ações ou funcionalidades 
+        public void ApresentarSe()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, meu RM é {RM}, nasci na data {DataNascimento}");
+        }
+
+    }
+}
