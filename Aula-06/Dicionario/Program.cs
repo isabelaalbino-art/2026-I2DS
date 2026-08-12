8304aa00e69349d2fed651399f716f74ba00d947
isabelaalbino-art <isabela.albino@edu.senai.br>
2026-08-12 11:15:08 -0300

    Aula 06 -  Colletions em C#

---
+++ Aula-06/Dicionario/Program.cs
@@ -0,0 +1,61 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=== TRABALHANDO COM LISTAS ===");
+        // Sintaxe Dictonary<chave, valor>
+        //                  <>
+        
+        Dictionary<string, string> dicionario = new Dictionary<string, string>();
+        // Adicionar elementos no dicionario
+        dicionario.Add("Casa","Construção destinada à moradia.");
+        dicionario.Add("Livro","Conjunto de páginas encadernadas contendo textos, imagens ou informações.");
+        dicionario.Add("Amizade","Relação de afeto, confiança e companheirismo entre pessoas.");
+        dicionario.Add("Escola","Instituição destinada ao ensino e à educação de crianças, jovens ou adultos.");
+
+        // 3 - Percorrer todos os elementos de eum dicionario
+        Console.WriteLine();
+        foreach(var palavra in dicionario)
+        {
+            Console.WriteLine($"{palavra.Key} - {palavra.Value}");
+        }
+
+        //Alterar um elemento do dicionario
+        dicionario["Casa"] = "Construção destinada à moradia definitiva ou temporaria.";
+        Console.WriteLine();
+        foreach(var palavra in dicionario)
+        {
+            Console.WriteLine($"{palavra.Key} - {palavra.Value}");
+        }
+
+        //Verificar se uma chave (key) dentro do dicionario
+        string termo;
+        bool chaveExiste = dicionario.TryGetValue("Amizade", out termo);
+        Console.WriteLine();
+        if (chaveExiste)
+        {
+            Console.WriteLine($"A palavra foi encontrada: {termo}");
+        } else
+        {
+            Console.WriteLine("A palavra mão foi encontrada no diciionário");
+        }
+
+        // 6 -Excluir um item do dicionario
+        dicionario.Remove("Casa");
+
+        Console.WriteLine();
+        foreach(var palavra in dicionario)
+        {
+            Console.WriteLine($"{palavra.Key} - {palavra.Value}");
+        }
+
+        // 7 - Ordenar um dicionario por chave
+         var DicionarioOrdenado = dicionario.OrderBy(i => i.Key); // (=>) - tal que 
+        Console.WriteLine();
+        foreach(var palavra in DicionarioOrdenado)
+        {
+            Console.WriteLine($"{palavra.Key} - {palavra.Value}");
+        }
+
+    }
+}
---
+++ Aula-06/Pilhas/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=== TRABALAHNDO COM PILHAS ( FILO ) ===");
+        
+      // 1 - Criar uma nova pilha vazia 
+      Stack<string> pilhaLivros = new Stack<string>();
+
+      // 2 - Adicionar elementos em uma pilha
+      pilhaLivros.Push("Dom Quixote");  
+      pilhaLivros.Push("O morro dos ventos uivantes");
+      pilhaLivros.Push("O cortiço");  
+
+      //Percprrer todos os elementos de uma pilha
+      Console.WriteLine();
+      foreach(var item in pilhaLivros)
+        {
+            Console.WriteLine(item);
+        }
+
+        // 4 - Remover um elemento da pilha
+       string livroRemovido = pilhaLivros.Pop(); // pop remove o primeiro elemento da pilha 
+       Console.WriteLine();
+       Console.WriteLine($"O livro retirado foi: {livroRemovido}");
+
+       pilhaLivros.Push("Dom Casmurro");
+       Console.WriteLine();
+       Console.WriteLine();
+       foreach(var item in pilhaLivros)
+        {
+            Console.WriteLine(item);
+        }
+
+
+      
+        }
+}
